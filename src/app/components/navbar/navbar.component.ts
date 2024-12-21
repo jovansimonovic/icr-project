7c7496864a9c3fc73be4e409b2dd83d7c3bdc05e
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-12-21 14:37:25 +0100

    feature: developed pushMessage and sendMessage functions

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { CartService } from '../../../services/cart.service';
 import { MessageModel } from '../../../models/message.model';
 import { ChatbotService } from '../../../services/chatbot.service';
+import { RasaModel } from '../../../models/rasa.model';
 
 @Component({
   selector: 'app-navbar',
@@ -20,6 +21,9 @@ export class NavbarComponent implements OnInit {
   userMessage: string = '';
   messages: MessageModel[] = [];
 
+  // grants access the #chatBody element directly
+  @ViewChild('chatBody', { static: false }) chatBody: ElementRef | undefined;
+
   constructor(
     private userService: UserService,
     private cartService: CartService,
@@ -33,17 +37,81 @@ export class NavbarComponent implements OnInit {
     this.cartService.cartItemCount$.subscribe(
       (count) => (this.cartItemCount = count)
     );
+
+    if (!localStorage.getItem('messages')) {
+      localStorage.setItem(
+        'messages',
+        JSON.stringify([{ type: 'bot', text: 'Hi! How can I help you?' }])
+      );
+    }
+
+    this.messages = JSON.parse(localStorage.getItem('messages')!);
   }
 
-  ngAfterViewChecked(): void {}
+  ngAfterViewChecked(): void {
+    // scrolls to bottom after the view has been updated
+    if (this.chatBody) {
+      this.chatBody.nativeElement.scrollTop =
+        this.chatBody.nativeElement.scrollHeight;
+    }
+  }
 
   toggleChat() {
     this.isChatVisible = !this.isChatVisible;
   }
 
-  pushMessage() {}
+  pushMessage(message: MessageModel) {
+    if (
+      message.type === 'bot' &&
+      message.text === this.botThinkingPlaceholder
+    ) {
+      this.waitingForResponse = true;
+    }
 
-  sendMessage() {}
+    if (
+      message.type === 'bot' &&
+      message.text !== this.botThinkingPlaceholder
+    ) {
+      // tries to find placeholder thinking message
+      for (let msg of this.messages) {
+        if (msg.type === 'bot' && msg.text === this.botThinkingPlaceholder) {
+          msg.text = message.text;
+          this.waitingForResponse = false;
+          return;
+        }
+      }
+    }
+
+    this.messages.push(message);
+
+    // saves messages in local storage
+    localStorage.setItem('messages', JSON.stringify(this.messages));
+  }
+
+  sendMessage() {
+    if (this.waitingForResponse) return;
+
+    if (this.userMessage.trim()) {
+      const trimmedInput = this.userMessage;
+
+      this.userMessage = '';
+
+      this.pushMessage({ type: 'user', text: trimmedInput });
+      this.pushMessage({ type: 'bot', text: this.botThinkingPlaceholder });
+
+      this.chatbotService
+        .getRasaMessage(trimmedInput)
+        .subscribe((response: RasaModel[]) => {
+          if (response.length === 0) {
+            this.pushMessage({
+              type: 'bot',
+              text: "I'm sorry, I didn't understand that. Could you please rephrase your question?",
+            });
+            return;
+          }
+        });
+    }
+  }
 
   logout() {
     this.userService.logout();
